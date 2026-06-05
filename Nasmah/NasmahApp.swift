37d35bf7890b7225d2d4b865430c2ada6731fb5d
esraaehab333 <esraaehabb333@gmail.com>
2026-06-05 20:58:47 +0200

    first commit

---
+++ Nasmah/NasmahApp.swift
@@ -0,0 +1,20 @@
+//
+//  NasmahApp.swift
+//  Nasmah
+//
+//  Created by Nemo on 05/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct NasmahApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
